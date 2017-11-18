ed3885be89b7152834aa21d19e32d623ecf2110a
Gatuk S Chattanon <gatukgl@gmail.com>
2017-11-18 16:09:55 +0700

    Use mongodb lab on cloud server

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 module.exports = {
     // mongoUri: 'mongodb://localhost/myFirstNode',
-    // or use 
-    mongoUri: 'mongodb://admin:password@localhost:27017/testDB1',
+    // or use
+    mongoUri: 'mongodb://admin:password@ds113636.mlab.com:13636/users',
     debug: true,
     sessionSecret: 'dev_secret_key'
 }
