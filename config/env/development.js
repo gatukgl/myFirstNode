92a53eda72572709dd7c5e4a98f341be50c9ad9c
NonSunday <nonpcn@gmail.com>
2017-11-18 23:17:41 +0700

    Update endpoint from Non

--- config/env/development.js
+++ config/env/development.js
@@ -5,8 +5,9 @@ module.exports = {
     debug: true,
     sessionSecret: 'dev_secret_key',
     google: {
-        clientID: '<CLIENT>',
-        clientSecret: '<CLIENT>',
-        callbackURL: 'http://localhost:3000/oauth/google/callback'
+        clientID: '649580014773-lov0s057rv49d1jpoajds2cqft1dptm7.apps.googleusercontent.com',
+        clientSecret: '1Im94qvZST0IPkfwSDXkaC33',
+        //         callbackURL: 'http://localhost:3000/oauth/google/callback'
+        callbackURL: 'https://test-deploy-node-app.herokuapp.com/oauth/google/callback'
     }
 }
